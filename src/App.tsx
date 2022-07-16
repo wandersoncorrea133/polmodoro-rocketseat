9e85129d1c6c0a4a950dccfc194eba3a4473f3b2
wanderson <wanderson_c_c@hotmail.com>
2022-07-16 20:41:58 -0300

    add global style and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { Home } from "./components/Home";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Home />
     </ThemeProvider>
   );
